8dc8d8eeb3ef611212dfdc14448009a76216d104
Lion Kortlepel <development@kortlepel.com>
2020-01-31 16:43:40 +0100

    Add more functions

--- bitops.h
+++ bitops.h
@@ -1,24 +1,40 @@
 #ifndef BITOPS_H
 #define BITOPS_H
 
+/// Flips the bit at position n
 template<typename _IntT, typename _SmallIntT>
-inline _IntT& bitflip(_IntT& i, _SmallIntT n) {
+inline _IntT& bit_flip(_IntT& i, _SmallIntT n) {
     return i ^= 1 << (n - 1);
 }
 
+/// Sets the bit at position n
 template<typename _IntT, typename _SmallIntT>
-inline _IntT& bitset(_IntT& i, _SmallIntT n) {
+inline _IntT& bit_set(_IntT& i, _SmallIntT n) {
     return i |= 1 << (n - 1);
 }
 
+/// Sets all bits in the mask in i
+template<typename _IntT>
+inline _IntT& bit_set_mask(_IntT& i, _IntT mask) {
+    return i |= mask;
+}
+
+/// Unsets the bit at position n
 template<typename _IntT, typename _SmallIntT>
-inline _IntT& bitunset(_IntT& i, _SmallIntT n) {
+inline _IntT& bit_unset(_IntT& i, _SmallIntT n) {
     return i &= ~(1 << (n - 1));
 }
 
+/// Checks whether the bit at position n is set
 template<typename _IntT, typename _SmallIntT>
-constexpr bool is_bit_set(const _IntT& i, _SmallIntT n) {
+inline constexpr bool is_bit_set(_IntT i, _SmallIntT n) {
     return i & (1 << (n - 1));
+}
+
+/// Checks whether all bits in the mask are set in i
+template<typename _IntT>
+inline constexpr bool is_bitmask_set(_IntT i, _IntT mask) {
+    return (i & mask) == mask;
 }
 
 #endif // BITOPS_H
--- tests/test_bitops.cpp
+++ tests/test_bitops.cpp
@@ -1,25 +1,25 @@
 #include "catch.hpp"
 #include "../bitops.h"
 
-TEST_CASE("bitflip") {
+TEST_CASE("bit_flip") {
     int x = 2;
     // flip a few times
-    REQUIRE(bitflip(x, 3) == 6);
+    REQUIRE(bit_flip(x, 3) == 6);
     REQUIRE(x == 6);
 
-    REQUIRE(bitflip(x, 3) == 2);
+    REQUIRE(bit_flip(x, 3) == 2);
     REQUIRE(x == 2);
 
-    REQUIRE(bitflip(x, 3) == 6);
+    REQUIRE(bit_flip(x, 3) == 6);
     REQUIRE(x == 6);
 
     // flip 0 to 1
     x = 0;
-    REQUIRE(bitflip(x, 1) == 1);
+    REQUIRE(bit_flip(x, 1) == 1);
     REQUIRE(x == 1);
 
     // flip 1 to 0 again
-    REQUIRE(bitflip(x, 1) == 0);
+    REQUIRE(bit_flip(x, 1) == 0);
     REQUIRE(x == 0);
 }
 
@@ -31,39 +31,88 @@ TEST_CASE("is_bit_set") {
     REQUIRE(is_bit_set(0b00000010, 1) == false);
 }
 
-TEST_CASE("bitset") {
+TEST_CASE("bit_set") {
     int x = 0;
     REQUIRE(is_bit_set(x, 1) == false);
-    bitset(x, 1);
+    bit_set(x, 1);
     REQUIRE(is_bit_set(x, 1) == true);
 
     // make sure it doesn't flip and really just sets by setting again
     // and expecting no difference in output
-    bitset(x, 1);
+    bit_set(x, 1);
     REQUIRE(is_bit_set(x, 1) == true);
 
     // now make sure the return value makes sense
     x     = 1;
-    int y = bitset(x, 2);
+    int y = bit_set(x, 2);
     REQUIRE(x == y);
 }
 
-TEST_CASE("bitunset") {
+TEST_CASE("bit_unset") {
     int x = 0b11;
-    bitunset(x, 1);
+    bit_unset(x, 1);
     REQUIRE(x == 0b10);
 
-    bitunset(x, 2);
+    bit_unset(x, 2);
     REQUIRE(x == 0b00);
 
     REQUIRE(x == 0);
 
     x = 0b10;
-    bitunset(x, 1);
+    bit_unset(x, 1);
     REQUIRE(x == 0b10);
 
     // test return value
     x     = 3;
-    int y = bitunset(x, 1);
+    int y = bit_unset(x, 1);
     REQUIRE(x == y);
+}
+
+TEST_CASE("is_bitmask_set") {
+    unsigned char i    = 0b00000000;
+    unsigned char mask = 0b01010101;
+
+    REQUIRE(is_bitmask_set(i, mask) == false);
+
+    i = mask;
+
+    REQUIRE(is_bitmask_set(i, mask) == true);
+
+    i = 0b11111111;
+
+    REQUIRE(is_bitmask_set(i, mask) == true);
+
+    bit_unset(i, 1);
+
+    REQUIRE(is_bitmask_set(i, mask) == false);
+
+    i    = 0;
+    mask = 0;
+
+    REQUIRE(is_bitmask_set(i, mask) == true);
+}
+
+
+TEST_CASE("bit_set_mask") {
+    unsigned char i    = 0;
+    unsigned char mask = 0b11111111;
+
+    bit_set_mask(i, mask);
+    REQUIRE(is_bitmask_set(i, mask) == true);
+
+    // make sure it doesn't flip
+    bit_set_mask(i, mask);
+    REQUIRE(is_bitmask_set(i, mask) == true);
+
+    i    = 0b10101010;
+    mask = 0b00000001;
+
+    bit_set_mask(i, mask);
+    REQUIRE(i == 0b10101011);
+
+    // check return value
+    i     = 0;
+    mask  = 3;
+    int y = bit_set(i, mask);
+    REQUIRE(i == y);
 }
